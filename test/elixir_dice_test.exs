c403c78c998f63ea44a26ab49d3da347eeef49b7
yank.nvim <yanknvim@gmail.com>
2024-05-28 18:43:25 +0900

    chore: Add test

--- test/elixir_dice_test.exs
+++ test/elixir_dice_test.exs
@@ -2,7 +2,8 @@ defmodule ElixirDiceTest do
   use ExUnit.Case
   doctest ElixirDice
 
-  test "greets the world" do
-    assert ElixirDice.hello() == :world
+  test "Test parser" do
+    assert elem(ElixirDice.Parse.dice("1d100"), 1) == [1, 100]
+    assert elem(ElixirDice.Parse.dice("45d6"), 1) == [45, 6]
   end
 end
